13312a0cbbc19c602d19dab416580ee61f2d5527
Raul Deus <jd.raul@gmail.com>
2024-03-08 15:28:04 -0300

    quase pronto

--- assets/js/script_.js
+++ assets/js/script_.js
@@ -223,7 +223,7 @@ function validaCPF(cpf) {
   resto = (soma * 10) % 11;
 
   if ((resto === 10) || (resto === 11)) resto = 0;
-  if (resto !== parseInt(cpf.substring(10, 11))) return false;
+  if (resto !== parseInt(cpf.substring(10, 11))) return false; 
 
   return true;
 }
